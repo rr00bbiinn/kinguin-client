5d83e86172e7ba684e6f4b9f188427bf73eed870
rr00bbiinn <robin@robinkarlsson.com>
2022-12-04 18:48:05 +0300

    refactor: add sandbox url and sandbox flag

--- kinguin_client/kinguin.py
+++ kinguin_client/kinguin.py
@@ -6,13 +6,17 @@ from kinguin_client.clients.products_client import ProductsClient
 class Kinguin:
 
     BASE_URL = "https://gateway.kinguin.net/esa/"
+    SANDBOX_URL = "https://gateway.sandbox.kinguin.net/esa/"
     API_PATH = "api/v1/"
 
-    def __init__(self, api_key: str = None):
+    def __init__(self, api_key: str = None, sandbox: bool = False):
         self.api_key = api_key
+        self.sandbox = sandbox
 
     @property
     def url(self):
+        if self.sandbox:
+            return f"{Kinguin.SANDBOX_URL}{Kinguin.API_PATH}"
         return f"{Kinguin.BASE_URL}{Kinguin.API_PATH}"
 
     def products(self):
--- tests/test_kinguin_client.py
+++ tests/test_kinguin_client.py
@@ -10,3 +10,9 @@ def test_kinguin_client():
 
     assert client.api_key == "api_key"
     assert client.url == "https://gateway.kinguin.net/esa/api/v1/"
+
+
+def test_kinguin_sandbox_client():
+    client = Kinguin(api_key="api_key", sandbox=True)
+
+    assert client.url == "https://gateway.sandbox.kinguin.net/esa/api/v1/"
